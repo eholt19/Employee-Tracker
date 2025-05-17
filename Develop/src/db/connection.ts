eb13f704512cd7f4c209d92c3076e91fc9e97418
Emily <eholt179@gmail.com>
2025-05-16 19:07:01 -0600

    trying to fix errors

--- Develop/src/db/connection.ts
+++ Develop/src/db/connection.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: 'localhost',
   database: process.env.DB_NAME,
--- Develop/src/db/schema.sql
+++ Develop/src/db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
+-- DROP DATABASE IF EXISTS employees_db;
+-- CREATE DATABASE employees_db;
 
-\c employees_db;
+-- \c employees_db;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
@@ -12,8 +12,7 @@ CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL,
-    -- CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    department_id INTEGER NOT NULL REFERENCES department(id)
 );
 
 CREATE TABLE employee (
